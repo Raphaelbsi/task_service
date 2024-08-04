ef599ce8397f5c6ee813d4783eca9373eb4e7f8b
Raphael <oliveira_raphael@msn.com>
2024-08-04 19:22:08 -0300

    adjust on init.sql

--- init.sql
+++ init.sql
@@ -1,12 +1,14 @@
 CREATE DATABASE IF NOT EXISTS main_app_db;
+CREATE DATABASE IF NOT EXISTS main_app_test;
 CREATE DATABASE IF NOT EXISTS auth_service_db;
+CREATE DATABASE IF NOT EXISTS auth_service_test;
 CREATE DATABASE IF NOT EXISTS notification_service_db;
 CREATE DATABASE IF NOT EXISTS scraping_service_db;
 
-CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'password';
-
 GRANT ALL PRIVILEGES ON main_app_db.* TO 'user'@'%';
+GRANT ALL PRIVILEGES ON main_app_test.* TO 'user'@'%';
 GRANT ALL PRIVILEGES ON auth_service_db.* TO 'user'@'%';
+GRANT ALL PRIVILEGES ON auth_service_test.* TO 'user'@'%';
 GRANT ALL PRIVILEGES ON notification_service_db.* TO 'user'@'%';
 GRANT ALL PRIVILEGES ON scraping_service_db.* TO 'user'@'%';
 
